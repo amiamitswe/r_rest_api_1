3830cfbadc291ac0c80d34ce7157b8153de0d969
Amit Samadder <amiamitswe@gmail.com>
2022-11-01 19:15:26 +0600

    update user and rescue if no user found

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,10 +12,7 @@ class Api::V1::UsersController < ApplicationController
       render json: @user
     else
       # render json: {error: 'unable to create user'}, stauts: 400
-      render json: {
-               errors: @user.errors,
-             },
-             status: 422
+      render json: { errors: @user.errors }, status: 422
     end
   end
 
@@ -23,6 +20,21 @@ class Api::V1::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    render json: @user
+    if stale?(last_modified: @user.updated_at)
+      render json: @user
+    end
+  end
+
+  # PUT /users/:id
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(params.require(:user).permit(:username, :password))
+      render json: { message: params[:id] + " Update successfull" }, status: 200
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  rescue ActiveRecord::RecordNotFound => error
+    render json: { message: "no user found", status: 404 }, status: 404
   end
 end
