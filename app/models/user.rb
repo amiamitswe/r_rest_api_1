4f84176b07504dc32cfbf713b58b93f90c088a2a
Amit Samadder <amiamitswe@gmail.com>
2022-11-02 16:55:23 +0600

    add bcrype and hashed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :facts
-  validates :username, presence: true, length: {minimum: 4, maximum: 20}, uniqueness: true
-  validates :password, presence: true, length: {minimum: 6, maximum: 20}
+  validates :username, presence: true, length: { minimum: 4, maximum: 20 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_063150) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_095937) do
   create_table "facts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "fact"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_063150) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "facts", "users"
